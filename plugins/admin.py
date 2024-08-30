47dff204ee87eacfda4e0bec37aad662a955870f
Jishu Developer <154415789+JishuDeveloper@users.noreply.github.com>
2024-08-30 20:28:51 +0530

    Update admin.py

--- plugins/admin.py
+++ plugins/admin.py
@@ -10,16 +10,16 @@ from helper.database import uploadlimit, usertype, addpre
 
 @Client.on_message(filters.private & filters.user(ADMIN) & filters.command(["warn"]))
 async def warn(c, m):
-        if len(m.command) >= 3:
-            try:
-                user_id = m.text.split(' ', 2)[1]
-                reason = m.text.split(' ', 2)[2]
-                await m.reply_text("User Notfied Sucessfully 😁")
-                await c.send_message(chat_id=int(user_id), text=reason)
-            except:
-                 await m.reply_text("User Not Notfied Sucessfully 😔")
-
-
+    if len(m.command) >= 3:
+        try:
+            user_id = m.text.split(' ', 2)[1]
+            reason = m.text.split(' ', 2)[2]
+            await m.reply_text("User Notfied Sucessfully 😁")
+            await c.send_message(chat_id=int(user_id), text=reason)
+        except:
+            await m.reply_text("User Not Notfied Sucessfully 😔")
+            
+            
 
 @Client.on_message(filters.private & filters.user(ADMIN) & filters.command(["addpremium"]))
 async def buypremium(bot, message):
@@ -61,27 +61,27 @@ async def resetpower(bot, message):
 # PREMIUM POWER MODE @JISHUDEVELOPER
 @Client.on_callback_query(filters.regex('vip1'))
 async def vip1(bot,update):
-	id = update.message.reply_to_message.text.split("/addpremium")
-	user_id = id[1].replace(" ", "")
-	inlimit  = 21474836500
-	uploadlimit(int(user_id),21474836500)
-	usertype(int(user_id),"🪙 Basic")
-	addpre(int(user_id))
-	await update.message.edit("Added Successfully To Premium Upload Limit 20 GB")
-	await bot.send_message(user_id,"Hey You Are Upgraded To Basic. Check Your Plan Here /myplan")
+    id = update.message.reply_to_message.text.split("/addpremium")
+    user_id = id[1].replace(" ", "")
+    inlimit  = 21474836500
+    uploadlimit(int(user_id),21474836500)
+    usertype(int(user_id),"🪙 Basic")
+    addpre(int(user_id))
+    await update.message.edit("Added Successfully To Premium Upload Limit 20 GB")
+    await bot.send_message(user_id, f"Hey {update.from_user.mention} \n\nYou Are Upgraded To <b>🪙 Basic</b>. Check Your Plan Here /myplan")
 
 
 
 @Client.on_callback_query(filters.regex('vip2'))
 async def vip2(bot,update):
-	id = update.message.reply_to_message.text.split("/addpremium")
-	user_id = id[1].replace(" ", "")
-	inlimit = 53687091200
-	uploadlimit(int(user_id), 53687091200)
-	usertype(int(user_id),"⚡ Standard")
-	addpre(int(user_id))
-	await update.message.edit("Added Successfully To Premium Upload Limit 50 GB")
-	await bot.send_message(user_id,"Hey You Are Upgraded To Standard. Check Your Plan Here /myplan")
+    id = update.message.reply_to_message.text.split("/addpremium")
+    user_id = id[1].replace(" ", "")
+    inlimit = 53687091200
+    uploadlimit(int(user_id), 53687091200)
+    usertype(int(user_id),"⚡ Standard")
+    addpre(int(user_id))
+    await update.message.edit("Added Successfully To Premium Upload Limit 50 GB")
+    await bot.send_message(user_id, f"Hey {update.from_user.mention} \n\nYou Are Upgraded To <b>⚡ Standard</b>. Check Your Plan Here /myplan")
 
 
 
@@ -94,7 +94,7 @@ async def vip3(bot,update):
 	usertype(int(user_id),"💎 Pro")
 	addpre(int(user_id))
 	await update.message.edit("Added Successfully To Premium Upload Limit 100 GB")
-	await bot.send_message(user_id,"Hey You Are Upgraded To Pro. Check Your Plan Here /myplan")
+	await bot.send_message(user_id, f"Hey {update.from_user.mention} \n\nYou Are Upgraded To <b>💎 Pro</b>. Check Your Plan Here /myplan")
 
 
 
@@ -103,27 +103,27 @@ async def vip3(bot,update):
 # CEASE POWER MODE @JISHUDEVELOPER
 @Client.on_callback_query(filters.regex('cp1'))
 async def cp1(bot,update):
-	id = update.message.reply_to_message.text.split("/ceasepower")
-	user_id = id[1].replace(" ", "")
-	inlimit  = 2147483652
-	uploadlimit(int(user_id), 2147483652)
-	usertype(int(user_id),"⚠️ Account Downgraded")
-	addpre(int(user_id))
-	await update.message.edit("Added Successfully To Upload Limit 2GB")
-	await bot.send_message(user_id,"Hey You Are Downgraded To Cease Limit 2GB. Check Your Plan Here /myplan \n\n**Contact Admin :** @calladminrobot")
+    id = update.message.reply_to_message.text.split("/ceasepower")
+    user_id = id[1].replace(" ", "")
+    inlimit  = 2147483652
+    uploadlimit(int(user_id), 2147483652)
+    usertype(int(user_id),"⚠️ Account Downgraded")
+    addpre(int(user_id))
+    await update.message.edit("Added Successfully To Upload Limit 2GB")
+    await bot.send_message(user_id, f"Hey {update.from_user.mention} \n\nYou Are Downgraded To Cease <b>Limit 2GB</b>. Check Your Plan Here /myplan \n\n<b>Contact Admin :</b> @MadflixOfficials")
 
 
 
 @Client.on_callback_query(filters.regex('cp2'))
 async def cp2(bot,update):
-	id = update.message.reply_to_message.text.split("/ceasepower")
-	user_id = id[1].replace(" ", "")
-	inlimit  = 0
-	uploadlimit(int(user_id), 0)
-	usertype(int(user_id),"⚠️ Account Downgraded")
-	addpre(int(user_id))
-	await update.message.edit("Added Successfully To Upload Limit 0GB")
-	await bot.send_message(user_id,"Hey You Are Downgraded To Cease Limit 0GB. Check Your Plan Here /myplan \n\n**Contact Admin :** @calladminrobot")
+    id = update.message.reply_to_message.text.split("/ceasepower")
+    user_id = id[1].replace(" ", "")
+    inlimit  = 0
+    uploadlimit(int(user_id), 0)
+    usertype(int(user_id),"⚠️ Account Downgraded")
+    addpre(int(user_id))
+    await update.message.edit("Added Successfully To Upload Limit 0GB")
+    await bot.send_message(user_id, f"Hey {update.from_user.mention} \n\nYou Are Downgraded To Cease <b>Limit 0GB</b>. Check Your Plan Here /myplan \n\n<b>Contact Admin :</b> @MadflixOfficials")
 
 
 
@@ -131,14 +131,14 @@ async def cp2(bot,update):
 # RESET POWER MODE @JISHUDEVELOPER
 @Client.on_callback_query(filters.regex('dft'))
 async def dft(bot,update):
-	id = update.message.reply_to_message.text.split("/resetpower")
-	user_id = id[1].replace(" ", "")
-	inlimit = 2147483652
-	uploadlimit(int(user_id), 2147483652)
-	usertype(int(user_id),"🆓 Free")
-	addpre(int(user_id))
-	await update.message.edit("**Daily Data Limit Has Been Reset Successfully.**\n\nThis Account Has Default 2GB Remaining Capacity")
-	await bot.send_message(user_id,"**Your Daily Data Limit Has Been Reset Successfully.**\n\nCheck Your Plan Here /myplan\n\n**Contact Admin :** <a href='https://t.me/CallAdminRobot'>Jishu Developer</a>")
+    id = update.message.reply_to_message.text.split("/resetpower")
+    user_id = id[1].replace(" ", "")
+    inlimit = 2147483652
+    uploadlimit(int(user_id), 2147483652)
+    usertype(int(user_id),"🆓 Free")
+    addpre(int(user_id))
+    await update.message.edit("Daily Data Limit Has Been Reset Successfully.\n\nThis Account Has Default 2GB Remaining Capacity")
+	await bot.send_message(user_id, f"Hey {update.from_user.mention} \n\nYour Daily Data Limit Has Been Reset Successfully. Check Your Plan Here /myplan\n\n<b>Contact Admin :</b> @MadflixOfficials")
 
 
 

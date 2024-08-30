c8174b88d23aec6db3b2df182e99c057f3b6e05b
Jishu Developer <154415789+JishuDeveloper@users.noreply.github.com>
2024-08-31 00:43:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,36 +1,50 @@
 import os
 
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+
+# Required Variables Config
 API_ID = int(os.environ.get("API_ID", ""))
 API_HASH = os.environ.get("API_HASH", "")
-OWNER = int(os.environ.get("OWNER", ""))
-BOT_USERNAME = os.environ.get('BOT_USERNAME', "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+ADMIN = int(os.environ.get("ADMIN", ""))
 
+
+# Premium 4GB Renaming Client Config
+STRING_API_ID = int(os.environ.get("STRING_API_ID", "0"))
+STRING_API_HASH = os.environ.get("STRING_API_HASH", "")
+STRING_SESSION = os.environ.get("STRING_SESSION", "")
+
+
+# Log & Force Channel Config
 FORCE_SUBS = os.environ.get("FORCE_SUBS", "")
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
 
-DB_URL = os.environ.get("DB_URL", "")
-DB_NAME = os.environ.get("DB_NAME", "madflixbotz")
 
-STRING = os.environ.get("STRING", "")
-BOT_PIC = os.environ.get("BOT_PIC", "https://graph.org/file/ad48ac09b1e6f30d2dae4.jpg")
-
+# Mongo DB Database Config
+DATABASE_URL = os.environ.get("DATABASE_URL", "")
+DATABASE_NAME = os.environ.get("DATABASE_NAME", "madflixbotz")
 
 
+# Other Variables Config
+START_PIC = os.environ.get("START_PIC", "https://graph.org/file/ad48ac09b1e6f30d2dae4.jpg")
 
 
-# if you need to add verify system then dm me on telegram
-# Check demo bots
+
+
+
+# If You Need To Add Verify System Then Message Me On Telegram
+# Check Demo Bots
 # https://t.me/FileRenameXBot
 # https://t.me/PremiumRenamerRobot
 # https://t.me/FileRenamerXRobot
-# token verification adding features is paid so if you want then dm me
+# Token Verification Adding Features Is Paid So If You Want Then Dm Me
 
 
 # SHORTNER_URL = os.environ.get("SHORTNER_URL", "")
 # SHORTNER_API = os.environ.get("SHORTNER_API", "")
 # TOKEN_TIMEOUT = os.environ.get("TOKEN_TIMEOUT", "")
+
+
 
 
 # Jishu Developer 

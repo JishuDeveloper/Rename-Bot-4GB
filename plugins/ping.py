d20ded1def7e6bd6a3cc7c01a6576ac93c79e206
Jishu Developer <154415789+JishuDeveloper@users.noreply.github.com>
2024-08-30 21:14:01 +0530

    Update ping.py

--- plugins/ping.py
+++ plugins/ping.py
@@ -3,13 +3,15 @@ from pyrogram import filters, Client
 
 CMD = ["/"]
 
+
+
 @Client.on_message(filters.command("ping", CMD))
 async def ping(_, message):
     start_t = time.time()
     rm = await message.reply_text("Pinging....")
     end_t = time.time()
     time_taken_s = (end_t - start_t) * 1000
-    await rm.edit(f"Ping 🔥!\n{time_taken_s:.3f} ms")
+    await rm.edit(f"Ping 🔥!\n\n{time_taken_s:.3f} ms")
     return time_taken_s
 
 

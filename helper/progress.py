8cfad56155a8eb2c36c44473cd2f82f631054bf9
Jishu Developer <154415789+JishuDeveloper@users.noreply.github.com>
2024-08-31 00:28:37 +0530

    Update progress.py

--- helper/progress.py
+++ helper/progress.py
@@ -1,12 +1,14 @@
 import math
 import time
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+
 
 
 async def progress_for_pyrogram(current, total, ud_type, message, start):
     now = time.time()
     diff = now - start
-    if round(diff % 10.00) == 0 or current == total:
+    if round(diff % 5.0) == 0 or current == total:
         percentage = current * 100 / total
         speed = current / diff
         elapsed_time = round(diff) * 1000
@@ -16,18 +18,19 @@ async def progress_for_pyrogram(current, total, ud_type, message, start):
         elapsed_time = TimeFormatter(milliseconds=elapsed_time)
         estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
 
-        progress = "[{0}{1}] \n**Progress**: {2}%\n".format(
-            ''.join(["▣" for i in range(math.floor(percentage / 5))]),
-            ''.join(["▢" for i in range(20 - math.floor(percentage / 5))]),
-            round(percentage, 2))
+        filled_blocks = math.floor(percentage / 5)
+        empty_blocks = 20 - filled_blocks
 
-        tmp = progress + "{0} of {1}\n**Speed**: {2}/s\n**ETA**: {3}\n".format(
+        progress_bar = "■" * filled_blocks + "□" * empty_blocks
+
+        tmp = PROGRESS_BAR.format(
+            round(percentage, 2),
             humanbytes(current),
             humanbytes(total),
             humanbytes(speed),
-            estimated_total_time if estimated_total_time != '' else "0 s"
+            estimated_total_time if estimated_total_time != '' else '0 s',
+            progress_bar
         )
-
         
         cancel_button = InlineKeyboardButton("✖️ Cancel ✖️", callback_data="cancel")
 
@@ -43,9 +46,9 @@ async def progress_for_pyrogram(current, total, ud_type, message, start):
 def humanbytes(size):
     if not size:
         return ""
-    power = 2**10
+    power = 2 ** 10
     n = 0
-    Dic_powerN = {0: ' ', 1: 'Ki', 2: 'Mi', 3: 'Gi', 4: 'Ti'}
+    Dic_powerN = {0: ' ', 1: 'K', 2: 'M', 3: 'G', 4: 'T'}
     while size > power:
         size /= power
         n += 1
@@ -57,9 +60,34 @@ def TimeFormatter(milliseconds: int) -> str:
     minutes, seconds = divmod(seconds, 60)
     hours, minutes = divmod(minutes, 60)
     days, hours = divmod(hours, 24)
-    tmp = ((str(days) + "d, ") if days else "") + \
-        ((str(hours) + "h, ") if hours else "") + \
-        ((str(minutes) + "m, ") if minutes else "") + \
-        ((str(seconds) + "s, ") if seconds else "") + \
-        ((str(milliseconds) + "ms, ") if milliseconds else "")
+    tmp = (
+        (str(days) + "d, ") if days else ""
+    ) + (
+        (str(hours) + "h, ") if hours else ""
+    ) + (
+        (str(minutes) + "m, ") if minutes else ""
+    ) + (
+        (str(seconds) + "s, ") if seconds else ""
+    ) + (
+        (str(milliseconds) + "ms, ") if milliseconds else ""
+    )
     return tmp[:-2]
+
+
+PROGRESS_BAR = """\
+{5}
+
+<b>📁 Size</b> : {1} | {2}
+<b>⏳️ Done</b> : {0}%
+<b>🚀 Speed</b> : {3}/s
+<b>⏰️ ETA</b> : {4} """
+
+
+
+
+
+# Jishu Developer 
+# Don't Remove Credit 🥺
+# Telegram Channel @Madflix_Bots
+# Back-Up Channel @JishuBotz
+# Developer @JishuDeveloper & @MadflixOfficials

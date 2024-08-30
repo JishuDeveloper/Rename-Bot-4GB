2e5fc939292236291b8fccefed72c49295edd9f6
Jishu Developer <154415789+JishuDeveloper@users.noreply.github.com>
2024-08-31 00:30:45 +0530

    Update caption.py

--- plugins/caption.py
+++ plugins/caption.py
@@ -2,31 +2,40 @@ from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from helper.database import *
 
+
+
+
+
 @Client.on_message(filters.private & filters.command('set_caption'))
 async def add_caption(client, message):
     if len(message.command) == 1:
-       return await message.reply_text("**Give Me A Caption To Set.\n\nExample:- `/set_caption 📕 Name ➠ : {filename} \n\n🔗 Size ➠ : {filesize} \n\n⏰ Duration ➠ : {duration}`**")
+       return await message.reply_text("<b><u>Give Me A Caption To Set</u></b>\n\n<b>Example :</b> <code>/set_caption 📕 Name ➠ : {filename} \n\n🔗 Size ➠ : {filesize} \n\n⏰ Duration ➠ : {duration}</code>")
     caption = message.text.split(" ", 1)[1]
     addcaption(int(message.chat.id), caption)
-    await message.reply_text("**Your Caption Successfully Added ✅**")
+    await message.reply_text("Your Caption Successfully Added ✅")
+
+
 
 @Client.on_message(filters.private & filters.command('del_caption'))
 async def delete_caption(client, message): 
     caption = find(int(message.chat.id))[1]
     if not caption:
-        await message.reply_text("**You Don't Have Any Custom Caption ❌**")
+        await message.reply_text("You Don't Have Any Custom Caption ❌")
         return
     delcaption(int(message.chat.id))
-    await message.reply_text("**Your Caption Successfully Deleted 🗑️**")
+    await message.reply_text("Your Caption Successfully Deleted 🗑️")
+                                      
+                                      
                                        
 @Client.on_message(filters.private & filters.command('see_caption'))
 async def see_caption(client, message): 
     caption = find(int(message.chat.id))[1]
     if caption:
-       await message.reply_text(f"<b><u>Your Caption:</b></u>\n\n`{caption}`")
+       await message.reply_text(f"<b><u>Your Caption:</b></u>\n\n<code>{caption}</code>")
     else:
-       await message.reply_text("**You Don't Have Any Custom Caption ❌**")
+       await message.reply_text("You Don't Have Any Custom Caption ❌")
           
+
 
 
 
@@ -34,4 +43,5 @@ async def see_caption(client, message):
 # Jishu Developer 
 # Don't Remove Credit 🥺
 # Telegram Channel @Madflix_Bots
-# Developer @JishuDeveloper
+# Back-Up Channel @JishuBotz
+# Developer @JishuDeveloper & @MadflixOfficials

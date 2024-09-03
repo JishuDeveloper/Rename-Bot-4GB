69938de9c2d1fa682594bc1303da3d54db07f0a1
Jishu Developer <154415789+JishuDeveloper@users.noreply.github.com>
2024-09-03 10:07:39 +0530

    Update ffmpeg.py

--- helper/ffmpeg.py
+++ helper/ffmpeg.py
@@ -53,6 +53,47 @@ async def take_screen_shot(video_file, output_directory, ttl):
 
 
 
+async def add_metadata(input_path, output_path, metadata, ms):
+    try:
+        await ms.edit("<i>I Found Metadata, Adding Into Your File ⚡</i>")
+        command = [
+            'ffmpeg', '-y', '-i', input_path, '-map', '0', '-c:s', 'copy', '-c:a', 'copy', '-c:v', 'copy',
+            '-metadata', f'title={metadata}',  # Set Title Metadata
+            '-metadata', f'author={metadata}',  # Set Author Metadata
+            '-metadata:s:s', f'title={metadata}',  # Set Subtitle Metadata
+            '-metadata:s:a', f'title={metadata}',  # Set Audio Metadata
+            '-metadata:s:v', f'title={metadata}',  # Set Video Metadata
+            '-metadata', f'artist={metadata}',  # Set Artist Metadata
+            output_path
+        ]
+        
+        process = await asyncio.create_subprocess_exec(
+            *command,
+            stdout=asyncio.subprocess.PIPE,
+            stderr=asyncio.subprocess.PIPE,
+        )
+        stdout, stderr = await process.communicate()
+        e_response = stderr.decode().strip()
+        t_response = stdout.decode().strip()
+        print(e_response)
+        print(t_response)
+
+        
+        if os.path.exists(output_path):
+            await ms.edit("<i>Metadata Has Been Successfully Added To Your File ✅</i>")
+            return output_path
+        else:
+            await ms.edit("<i>Failed To Add Metadata To Your File ❌</i>")
+            return None
+    except Exception as e:
+        print(f"Error occurred while adding metadata: {str(e)}")
+        await ms.edit("<i>An Error Occurred While Adding Metadata To Your File ❌</i>")
+        return None
+    
+
+
+
+
 
 
 # Jishu Developer 
